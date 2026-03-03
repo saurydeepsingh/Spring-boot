1f95632dd01bab05602a2514d1455b6a9f3364be
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-03-04 00:03:24 +0530

    Add files via upload

--- StudentRepo.java
+++ StudentRepo.java
@@ -4,6 +4,11 @@ import com.Spring.jpa.Spring.data.jpa.ex2.Model.Studentjpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepo extends JpaRepository<Studentjpa, Integer> {
+     List<Studentjpa> findbyname(String name);
+     List<Studentjpa> findbymarks(int marks);
+
 }
