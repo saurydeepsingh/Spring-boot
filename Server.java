33c3710b0f8f43fb4d1385414d7a7ee2b4fece9a
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-03-05 22:10:22 +0530

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,13 +1,50 @@
 package com.example.CSE.CSE5.SPRING;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 
 public class Server {
+    List<topic> sarvil=new ArrayList<>(
+            Arrays.asList(
+                    new topic("101","saurydeep singh","address"),
+                    new topic("102","Sarvil","dehradun"),
+                    new topic("103","sanjana singh","Jamshedpur"),
+                    new topic("104","Saksham","dehradun")
+
+
+
+            )
+    );
+
     @RequestMapping("/")
     public String hello(){
         return "hello mam";
     }
+
+
+
+    @RequestMapping("/sarvilhello")
+    public List<topic> sarvil(){
+        return sarvil;
+
+    }
+    @RequestMapping("/sarvilhello/{myid}")
+    public topic hell(@PathVariable String myid){
+        Optional<topic> foundtopic= sarvil.stream().filter(t -> t.getId().equals(myid)).findFirst();
+        if(foundtopic.isPresent()){
+            return foundtopic.get();
+        }
+        else{
+            return new topic("N/A","N/A","N/A");
+        }
+    }
+
 }
